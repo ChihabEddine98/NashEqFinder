92181eaba8edbe2a33bc497d558ee26c5f05e9be
Chihab Eddine <ga_benamara@esi.dz>
2020-10-19 18:19:03 +0200

    install np and plt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     print("Yooooho let's go !")
+    plt.plot(np.linspace(-1,3),np.linspace(-2,2))
+    plt.show()
