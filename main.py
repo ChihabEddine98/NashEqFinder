cba1b38f51e46e98e2b0a3bfbd62670ce5c438ed
Chihab Eddine <ga_benamara@esi.dz>
2020-10-22 15:11:23 +0200

    all possible pure NashEqu fix

--- main.py
+++ main.py
@@ -24,15 +24,18 @@ class Solver:
         possible_nash2=[]
         # Best responses for Player 1 against Player 2 strategies
         for j in range(DIM[1]):
-            x_max=max(self.M[:,j],key= lambda x:x[0])[0]
-            best_reply1=[tuple(x) for x in self.M[:,j] if x[0]==x_max]
+            x_max=max(self.M[:,j],key= lambda x:x[0])
+            a=[tuple(x) for x in self.M[:,j]]
+            best_reply1=[(tuple(x),(a.index(tuple(x)),j)) for x in self.M[:,j] if x[0]==x_max[0]]
             possible_nash1+=best_reply1
         print(possible_nash1)
 
         # Best responses for Player 2 against Player 1 strategies
         for i in range(DIM[0]):
-            y_max = max(self.M[i,:], key=lambda x: x[1])[1]
-            best_reply2 = [tuple(y) for y in self.M[i,:] if y[1] == y_max]
+            y_max = max(self.M[i,:], key=lambda y: y[1])
+            a=[tuple(x) for x in self.M[i,:]]
+            best_reply2=[(tuple(y),(i,a.index(tuple(y)))) for y in self.M[i,:] if y[1]==y_max[1]]
+            # best_reply2 = [tuple(y) for y in self.M[i,:] if y[1] == y_max]
             possible_nash2 += best_reply2
         print(possible_nash2)
         # Intersection of two lists give us the Nash equilibria !
